0e831adc665338eb3184a135989eb6a6aa2b82db
Olumide Olubosede <olumideolubosede@gmail.com>
2025-02-26 11:59:24 +0100

    Changed interstitial to rewarded ads

--- lib/src/screens/helpers/ad_helper.dart
+++ lib/src/screens/helpers/ad_helper.dart
@@ -1,11 +1,23 @@
 import 'dart:io';
 
 class AdHelper {
-  static const String testAdUnitId = '/21775744923/example/interstitial';
+  static const String testInterstitialAdUnitId =
+      '/21775744923/example/interstitial';
+
+  static const String testRewardedAdUnitId =
+      'ca-app-pub-3940256099942544/5224354917';
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
       return 'ca-app-pub-8930570865147939/5249034236';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-8930570865147939/6218237647';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
--- lib/src/screens/home_screen/channels_screen.dart
+++ lib/src/screens/home_screen/channels_screen.dart
@@ -17,49 +17,71 @@ class ChannelsScreen extends StatefulWidget {
 
 class _ChannelsScreenState extends State<ChannelsScreen> {
   AdManagerInterstitialAd? _interstitialAd;
+  RewardedAd? _rewardedAd;
 
   int? selectedTabIndex;
+
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.only(bottom: 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 5),
-              Align(
-                alignment: Alignment.center,
-                child: SizedBox(
-                  height: 120,
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    fit: BoxFit.scaleDown,
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.only(bottom: 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 5),
+                  Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      height: 120,
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                        fit: BoxFit.scaleDown,
+                      ),
+                    ),
                   ),
-                ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Channels',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  for (int i = 0; i < dummyChannels.length; i++)
+                    ChannelDisplay(
+                      index: i,
+                      isSelected:
+                          selectedTabIndex != null
+                              ? i == selectedTabIndex
+                              : false,
+                      onTap:
+                          () =>
+                              _onChangeTabPressed(context, i, dummyChannels[i]),
+                    ),
+                ],
               ),
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  'Channels',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
-                ),
-              ),
-              const SizedBox(height: 5),
-              for (int i = 0; i < dummyChannels.length; i++)
-                ChannelDisplay(
-                  index: i,
-                  isSelected:
-                      selectedTabIndex != null ? i == selectedTabIndex : false,
-                  onTap:
-                      () => _onChangeTabPressed(context, i, dummyChannels[i]),
-                ),
-            ],
+            ),
           ),
-        ),
+          isLoading
+              ? Positioned.fill(
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.white.withAlpha(150),
+                  child: CircularProgressIndicator(),
+                ),
+              )
+              : SizedBox(),
+        ],
       ),
     );
   }
@@ -72,28 +94,70 @@ class _ChannelsScreenState extends State<ChannelsScreen> {
     if (channelIndex == selectedTabIndex) {
       return;
     }
-    await AdManagerInterstitialAd.load(
-      adUnitId: AdHelper.testAdUnitId,
-      request: const AdManagerAdRequest(),
-      adLoadCallback: AdManagerInterstitialAdLoadCallback(
-        onAdLoaded: (ad) async {
-          debugPrint('$ad loaded.');
-          _interstitialAd = ad;
-          await _interstitialAd?.show();
+    // await AdManagerInterstitialAd.load(
+    //   adUnitId: AdHelper.testInterstitialAdUnitId,
+    //   request: const AdManagerAdRequest(),
+    //   adLoadCallback: AdManagerInterstitialAdLoadCallback(
+    //     onAdLoaded: (ad) async {
+    //       debugPrint('$ad loaded.');
+    //       _interstitialAd = ad;
+    //       await _interstitialAd?.show();
+    //
+    //       setState(() {
+    //         selectedTabIndex = channelIndex;
+    //       });
+    //       if (context.mounted) {
+    //         Navigator.pushNamed(
+    //           context,
+    //           MessagesScreen.screenId,
+    //           arguments: {'channel_name': channel.name},
+    //         );
+    //       }
+    //     },
+    //     onAdFailedToLoad: (LoadAdError error) {
+    //       debugPrint('AdManagerInterstitialAd failed to load: $error');
+    //     },
+    //   ),
+    // );
 
+    setState(() {
+      isLoading = true;
+    });
+
+    RewardedAd.load(
+      adUnitId: AdHelper.testRewardedAdUnitId,
+      request: const AdRequest(),
+      rewardedAdLoadCallback: RewardedAdLoadCallback(
+        onAdLoaded: (ad) async {
           setState(() {
-            selectedTabIndex = channelIndex;
+            isLoading = false;
           });
-          if (context.mounted) {
-            Navigator.pushNamed(
-              context,
-              MessagesScreen.screenId,
-              arguments: {'channel_name': channel.name},
-            );
-          }
+
+          debugPrint('$ad loaded.');
+          _rewardedAd = ad;
+          _rewardedAd?.show(
+            onUserEarnedReward: (AdWithoutView ad, RewardItem rewardItem) {},
+          );
+
+          ad.fullScreenContentCallback = FullScreenContentCallback(
+            onAdDismissedFullScreenContent: (ad) {
+              setState(() {
+                selectedTabIndex = channelIndex;
+              });
+              if (context.mounted) {
+                Navigator.pushNamed(
+                  context,
+                  MessagesScreen.screenId,
+                  arguments: {'channel_name': channel.name},
+                );
+              }
+
+              ad.dispose();
+            },
+          );
         },
         onAdFailedToLoad: (LoadAdError error) {
-          debugPrint('AdManagerInterstitialAd failed to load: $error');
+          debugPrint('RewardedAd failed to load: $error');
         },
       ),
     );
